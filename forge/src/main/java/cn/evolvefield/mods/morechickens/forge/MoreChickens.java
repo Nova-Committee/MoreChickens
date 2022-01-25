b706ba285a7f2c6ece4ad650ee7bb48f7b790870
Peakstep233 <William27060827@hotmail.com>
2022-01-25 20:04:56 +0800

    Small fix
    Ahh I'm gonna be insane!!!!!!!!!!!

--- forge/src/main/java/cn/evolvefield/mods/morechickens/forge/MoreChickens.java
+++ forge/src/main/java/cn/evolvefield/mods/morechickens/forge/MoreChickens.java
@@ -5,7 +5,7 @@ import cn.evolvefield.mods.morechickens.MoreChickensCommon;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(ExampleMod.MOD_ID)
+@Mod(MoreChickensCommon.MOD_ID)
 public class MoreChickens {
     public MoreChickens() {
         // Submit our event bus to let architectury register our content on the right time
